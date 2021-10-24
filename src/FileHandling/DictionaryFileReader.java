001f1e68660dba4e64702ad3a761430ca249b127
brandonrq506 <darkrahd@gmail.com>
2021-10-24 09:23:17 -0600

    Testing this will work

--- src/FileHandling/DictionaryFileReader.java
+++ src/FileHandling/DictionaryFileReader.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class DictionaryFileReader implements IFileHandler{
     
+    //File should be able to read mutliple lines
     private File archivo;
     private ExplorerFile fiEx;
     private Scanner sc;
