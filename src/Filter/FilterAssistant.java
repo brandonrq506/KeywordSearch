27bede31436f36b038d3e80920759366781dcdc6
brandonrq506 <darkrahd@gmail.com>
2021-10-27 16:53:02 -0600

    I feel lost in the darkness

--- src/Filter/FilterAssistant.java
+++ src/Filter/FilterAssistant.java
@@ -1,15 +1,16 @@
 package Filter;
 
+import Conversations.*;
+import FileHandling.Dictionary.Dictionary;
 import Filter.Behavior.FilterBehavior;
 import Filter.MessageSelector.MessageSelector;
+import java.util.List;
 
 /**
- * Init with MessageSelector & FilterBehavior 
- * Sets the FilterBehavior to our
+ * Init with MessageSelector & FilterBehavior Sets the FilterBehavior to our
  * FilterBehavior
  *
- * Receive the conversation 
- * Retrieves the messages we need from the conversation
+ * Receive the conversation Retrieves the messages we need from the conversation
  * Process the Filter() method If true adds the information we need to the
  * conversation Sends the conversation to the 'match; queue
  *
@@ -20,31 +21,48 @@ public class FilterAssistant implements Runnable {
     FilterBehavior fBehavior;
     MessageSelector mSelector;
     FilterManager fManager;
+    List<Dictionary> dictionaries;
+    
+    Conversation currentConversation;
+    List<Message> currentMessages;
 
     public FilterAssistant(FilterBehavior fBehavior,
             MessageSelector mSelector,
-            FilterManager fManager) {
+            FilterManager fManager,
+            List<Dictionary> dictionaries) {
         this.fBehavior = fBehavior;
         this.mSelector = mSelector;
         this.fManager = fManager;
+        this.dictionaries = dictionaries;
     }
 
     @Override
     public void run() {
-        //Get a conversation
-        //Get messages for that conversation
-        //While loop for all dictionaries
+        try {
+            getConversation();
+            setMessages();
+            for(Dictionary dictionary : this.dictionaries){
+                
+            }
+            
+            
+            
+            
+            
+        } catch (InterruptedException ex) {
+            System.out.println("Error on a Thread");
+        }
     }
-    
-    private void getConversation(){
-        //FilterManager will have a SYNC method that provides us with converstion
+
+    private void getConversation() throws InterruptedException {
+        this.currentConversation = this.fManager.getConversation();
     }
-    
-    private void setMessages(){
-        //We use this in our fBehavior initializer
+
+    private void setMessages() {
+        this.currentMessages = this.mSelector.getMessages(currentConversation);
     }
-    
-    private void filter(){
+
+    private void filter() {
         this.fBehavior.filter();
     }
 }
--- src/Filter/FilterManager.java
+++ src/Filter/FilterManager.java
@@ -7,66 +7,74 @@ import Filter.MessageSelector.MessageSelector;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
-//Gets Inicialized
-//Initializes BlockingQueues
-//Creates Assistants
-//Provides a method for assistants to get messages
-
 public class FilterManager {
-    
-    List<Dictionary> dictionaries;
-    FilterBehavior filterBehavior;
-    MessageSelector messageSelector;
-    FilterAssistant assistant;
-    
-    BlockingQueue<Conversation> enteringMessages;
-    BlockingQueue<Conversation> matchedMessages;
-    BlockingQueue<Conversation> notFoundMessages;
-    
-    ExecutorService threadPool;
-    
-    
+
+    //This is all for our Assistants
+    private List<Dictionary> dictionaries;
+    private FilterBehavior filterBehavior;
+    private MessageSelector messageSelector;
+    private FilterAssistant assistant;
+
+    //This is our responsability
+    private final BlockingQueue<Conversation> enteringMessages;
+    private final BlockingQueue<Conversation> matchedMessages;
+    private final BlockingQueue<Conversation> notFoundMessages;
+
+    //This is just to handle our threads
+    private final ExecutorService threadPool;
+    private static final int ASSISTANTS = 10;
+
     //Initializes the 2 Queues we are going to use
-    public FilterManager(){
+    public FilterManager() {
         this.enteringMessages = new LinkedBlockingDeque<>();
         this.matchedMessages = new LinkedBlockingDeque<>();
         this.notFoundMessages = new LinkedBlockingDeque<>();
-    }
-    
-    public void setFilterBehavior(FilterBehavior filterBehavior){
-        this.filterBehavior = filterBehavior;
-    }
-    
-    public void setMessageSelector(MessageSelector messageSelector){
-        this.messageSelector = messageSelector;
-    }
-    
-    public void setDictionaries(List<Dictionary> dictionaries){
-        this.dictionaries = dictionaries;
-    }
-    
-    public void Run(Conversation conversation){
-        createAssitants();
+        threadPool = Executors.newCachedThreadPool();
     }
 
-    private void createAssitants(){
-        //This class does not pass on a conversation
-        //The assitants grab the converstion by their own
-        //Here we need to have a pool of assitants so they get re-used     
+    public void setFilterBehavior(FilterBehavior filterBehavior) {
+        this.filterBehavior = filterBehavior;
     }
-    
-    protected Conversation getConversation() throws InterruptedException{
+
+    public void setMessageSelector(MessageSelector messageSelector) {
+        this.messageSelector = messageSelector;
+    }
+
+    public void setDictionaries(List<Dictionary> dictionaries) {
+        this.dictionaries = dictionaries;
+    }
+
+    public void Run(Conversation conversation) {
+        createAssitants();
+        //I create assistants before entering the while(exread.hasNext) loop
+        //while in the exread loop the assistants will handle the requests
+        //Once over, ex.write will just get a copy of the queues
+    }
+
+    private void createAssitants() {
+        for (int i = 0; i < ASSISTANTS; i++) {
+            this.threadPool.submit(new FilterAssistant(
+                    this.filterBehavior,
+                    this.messageSelector,
+                    this,
+                    this.dictionaries
+            ));
+        }
+    }
+
+    protected Conversation getConversation() throws InterruptedException {
         return this.enteringMessages.poll(100, TimeUnit.MILLISECONDS);
     }
-    
-    public void addConversationToMatch(Conversation conversation){
+
+    public void addConversationToMatch(Conversation conversation) {
         this.matchedMessages.add(conversation);
     }
-    
-    public void addConversationToNotMatch(Conversation conversation){
+
+    public void addConversationToNotMatch(Conversation conversation) {
         this.notFoundMessages.add(conversation);
     }
 }
